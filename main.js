17ae0726be654ac9de9e8766e3bcb6d5eceecae9
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-08-26 14:57:04 -0400

    started parallax effect

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 $(function() {
 	// cache window object (makes it run faster)
 	var $window = $(window);
+	
+	// parallax background
+	$('section[data-type="background"]').each(function() {
+		var $bgobj = $(this); // assigning the background object
+	});
 });
