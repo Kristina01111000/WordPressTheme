97fb53fc23b575cdf6d6323d25b7722340c15fb5
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-11-10 14:00:49 -0500

    fixed? navbar array problem

--- header.php
+++ header.php
@@ -43,14 +43,22 @@
 							</button>
 						</div>
 						<?php  //put the classes in the second part of array
-							wp_nav-menu( array(
+							/*wp_nav-menu( array(
 								'theme-location' 	=> 'primary',
 								'container'			=> 'nav',
 								'container-class'	=> 'navbar-collapse collapse',
 								'menu-class' 		=> 'nav navbar-nav navbar-right ml-auto'
 							)
 							
+							);*/
+							// check to see if this works
+							$args = array(
+								'theme-location' 	=> 'primary',
+								'container'		=> 'nav',
+								'container-class'	=> 'navbar-collapse collapse',
+								'menu-class' 		=> 'nav navbar-nav navbar-right ml-auto'
 							);
+							wp_nav-menu($args);
 						?>
 						<!-- original html code
 						<div class="navbar-collapse collapse ">
