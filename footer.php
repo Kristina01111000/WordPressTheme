914d9c3170881d390de38bd0305b30a84a37a336
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-11-30 16:19:57 -0500

    added directory

--- footer.php
+++ footer.php
@@ -14,8 +14,8 @@
 <?php wp_footer(); ?>
 	<!-- BOOTSTRAP CORE JAVASCRIPT -->		
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    	<script src="assets/js/jquery-3.5.1.min.js"</script>
+    	<script src="<?php bloginfo('template-directory');?>assets/js/jquery-3.5.1.min.js"</script>
     	<script src="assets/js/main.js"></script>
-	    <script src="assets/js/bootstrap.min.js"></script> 
+	<script src="assets/js/bootstrap.min.js"></script> 
 </body>
 </html>
